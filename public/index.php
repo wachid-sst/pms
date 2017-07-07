65645d53427fe8c27b31f62ef079cf4dc9d8f382
root <root@localhost>
2017-07-07 23:12:42 +0700

    part one
    arrage slim for pretty ease and mvc friendly

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+require '../bootstrap/app.php';
+require '../bootstrap/container.php';
+
+require '../routes/web.php';
+
+$app->run();
---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+// For system routing
+
+$app->get('/',  function() {
+    return 'Halo';
+});
